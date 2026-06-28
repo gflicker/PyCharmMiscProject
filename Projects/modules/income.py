f1f756ffc407815aa47883044d8db57b7cf8e384
Givious <gflixes@gmail.com>
2026-06-29 00:14:16 +0200

    Added only the expense tracker that am working on

--- Projects/modules/income.py
+++
@@ -1,9 +0,0 @@
-#This is a program to help with financial management
-def total_earnings():
-    print("Welcome to your personal Smart Expense Tracker")
-    Day_job = int(input("Salary K"))
-    Side_hustle = int(input("Side Hustle K"))
-    Freelancing = int(input("Freelancing K"))
-    # Total Net Income
-    Total_Net_Income = Day_job + Side_hustle + Freelancing
-    print(f'\tYour Total Net Income is: {Total_Net_Income}')
