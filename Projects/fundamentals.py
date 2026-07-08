09067defe236aacdd738cabb772df043e36d8812
Givious <gflixes@gmail.com>
2026-07-08 23:24:51 +0200

    add updated fundamentals module

--- Projects/fundamentals.py
+++ Projects/fundamentals.py
@@ -1,33 +1,62 @@
 class Fundamentals:
     """Class to represent the fundamentals class"""
-    def __init__(self, name, amount):
+    def __init__(self):
         """Initializing the fundamentals class"""
-        self.name = name
-        self.amount = amount
         self.expenses = {}
-
+        
     def get_expenses(self):
         """Getting the description and amount of each expense and add them to a dictionary"""
         enter = True
         while enter:
-            self.name = input("Enter name of the expense (q to quit): ")
-            self.name = self.name.lower()
-            if self.name == 'q':
+            name = input("Enter name of the expense (q to quit): ")
+            name = name.lower()
+            if name == 'q':
                 enter = False
             else:
-                self.amount = int(input("Enter amount (q to quit): "))
-                self.expenses[self.name] = self.amount
-
+                while True:
+                    enter_amount = input("Enter amount (q to quit): ")
+                    if enter_amount == "q":
+                        enter = False
+                        break
+                    try:
+                        amount = int(enter_amount)
+                        self.expenses[name] = amount
+                        break
+                    except ValueError:
+                        print('Please enter a numeric value')
 
     def expenses_summary(self):
         """Printing out all expenses that have been added to the expenses dictionary"""
-        print('\t=================================')
+        total_line = "_______________________________________"
+        total_padding = "Sub Total:"
         for key, value in self.expenses.items():
             key = key.title()
-            print(f"\t{key}                K{value}")
+            print(f"{key:<35} K{value}")
+        total = self.total_expenses()
+        print(f'{total_line:^40}')
+        print(f"{total_padding:<35} K{total}")
 
     def total_expenses(self):
         """Adding up each value in the dictionary of expenses to get the total"""
         total = sum(self.expenses.values())
-        print(f'\t__________________________________')
-        print(f'\tTotal                        K{total}')
+        return total
+
+    def get_fundamental_category(self):
+        fundamentals_banner = "Fundamental Expenses"
+        print(f'\n{fundamentals_banner:^40}')
+
+class Fun(Fundamentals):
+    """Class to represent the fun or recreation expenses"""
+    def get_fun_category(self):
+        """Getting the expenses category of the fun"""
+        recreation_banner = "Recreation Expenses"
+        print(f'\n{recreation_banner:^40}')
+
+
+class Savings(Fundamentals):
+    """Class to represent the savings expenses"""
+    def get_saving_category(self):
+        """Getting the savings name category and center align it"""
+        savings_banner = "Savings"
+        print(f'\n{savings_banner:^40}')
+
